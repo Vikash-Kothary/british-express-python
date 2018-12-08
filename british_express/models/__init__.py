04a7cff91caeb7ff0fe65af6eedbd454a42c7367
Vikash Kothary <kothary.vikash@gmail.com>
2018-12-08 17:38:46 +0000

    Create models

---
+++ british_express/models/__init__.py
@@ -0,0 +1,2 @@
+from british_express.models.user import User
+from british_express.models.picture import Picture
---
+++ british_express/models/pictures.py
@@ -0,0 +1,10 @@
+
+class Picture(object):
+
+    def __init__(self):
+        self.url = None
+        self.no_of_likes = None
+        self.pixel_array = None
+        self.predicted_likes = None
+        self.tags = None
+        self.user_id = None
